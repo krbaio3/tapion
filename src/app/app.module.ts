7dc9506a2b3e05c93b353004c7e38e97c0ad565b
KrBaIO3 <krbaio3@gmail.com>
2018-08-25 13:57:26 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
+// Componentes Personalizados
 import { AppComponent } from './app.component';
+
+// Modulos Personalizados
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
